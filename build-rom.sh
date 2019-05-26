fd49636e535c673780721c0143cc954818b81e0e
Ralph Garcia <32473502+proganime1200@users.noreply.github.com>
2019-05-26 11:53:39 +0800

    Update build-rom.sh

--- build-rom.sh
+++ build-rom.sh
@@ -69,7 +69,7 @@ repo sync -c -j$jobs --force-sync
 rm -f device/*/sepolicy/common/private/genfs_contexts
 (cd device/phh/treble; git clean -fdx; bash generate.sh $rom)
 
-sed -i -e 's/BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736/BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648/g' device/phh/treble/phhgsi_arm64_a/BoardConfig.mk
+sed -i -e 's/BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736/BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824/g' device/phh/treble/phhgsi_arm64_a/BoardConfig.mk
 
 if [ -f vendor/rr/prebuilt/common/Android.mk ];then
     sed -i \
